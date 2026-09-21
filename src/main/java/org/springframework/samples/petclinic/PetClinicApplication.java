501bf710e53c89116b8d33a68ffb0f3a255917cf
sabareeshrao <sabareeshrao@gmail.com>
2026-09-20 20:26:10 -0500

    P005 - Add Java main method

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -4,4 +4,9 @@ package org.springframework.samples.petclinic;
 // Batch - 004 Part 2: Define the first Java class for the project.
 public class PetClinicApplication {
 
+    // Batch - 005 Part 1: Add the Java application entry point.
+    public static void main(String[] args) {
+
+    }
+
 }
