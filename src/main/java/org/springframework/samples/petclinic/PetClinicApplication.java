b5e7d1de782a70b2d8f63db813e4e39d14dcbd9d
sabareeshrao <sabareeshrao@gmail.com>
2026-09-20 20:18:22 -0500

    P004 - Add first Java class with batch markers

---
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -0,0 +1,7 @@
+// Batch - 004 Part 1: Declare the Java package for the PetClinic application class.
+package org.springframework.samples.petclinic;
+
+// Batch - 004 Part 2: Define the first Java class for the project.
+public class PetClinicApplication {
+
+}
